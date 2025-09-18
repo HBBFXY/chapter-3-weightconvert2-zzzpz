6fbf4916cab7543539784864507d78e64760c336
zzzpz <1911293650@qq.com>
2025-09-18 13:34:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import re
+
 def main():
-    initial_weight = 60.0
+    initial_weight = float(input("请输入初始体重(kg): "))
     print("Year\tEarth(kg)\tMoon(kg)")
-    for year in range(10):  # 0到9年，共10年
+    for year in range(10):
         earth_weight = initial_weight + 0.5 * year
         moon_weight = earth_weight * 0.165
         print(f"{year}\t{earth_weight:.2f}\t\t{moon_weight:.2f}")
+
 if __name__ == "__main__":
     main()
