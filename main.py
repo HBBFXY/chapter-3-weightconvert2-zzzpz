cfacd85bd9a0287e743c09b81a0aaf3c4dd65f0f
zzzpz <1911293650@qq.com>
2025-09-18 13:41:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 def main():
     initial_weight = 60.0
-    print("Year\tEarth(kg)\tMoon(kg)")
+    print("年份\t地球体重(kg)\t月球体重(kg)")
     for year in range(10):
         earth_weight = initial_weight + 0.5 * year
         moon_weight = earth_weight * 0.165
-        print(f"{year}\t{earth_weight:.2f}\t\t{moon_weight:.2f}")
+        print(f"{year}\t{earth_weight:.1f}\t\t{moon_weight:.3f}")
 
 if __name__ == "__main__":
     main()
